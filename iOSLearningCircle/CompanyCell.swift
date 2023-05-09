ad83272851580b9a56b6db0052119bee2c11ece6
AlbertoGarciaWGU <107955268+AlbertoGarciaWGU@users.noreply.github.com>
2023-05-09 14:34:40 -0600

    Create 'CompanyCell'

---
+++ iOSLearningCircle/CompanyCell.swift
@@ -0,0 +1,15 @@
+//
+//  CompanyCell.swift
+//  iOSLearningCircle
+//
+//  Created by Alberto Garcia on 09/05/23.
+//
+
+import UIKit
+
+class CompanyCell: UITableViewCell {
+
+    @IBOutlet weak var companyImageView: UIImageView!
+    @IBOutlet weak var companyNameLabel: UILabel!
+    
+}
