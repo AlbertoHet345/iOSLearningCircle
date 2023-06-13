e864611862e40c44caa0275fcd9d02b820d1ef15
AlbertoGarciaWGU <107955268+AlbertoGarciaWGU@users.noreply.github.com>
2023-06-13 17:03:58 -0600

    Finish class 13/06/2023

---
+++ iOSLearningCircle/Service/HTTPClient.swift
@@ -0,0 +1,48 @@
+//
+//  HTTPClient.swift
+//  iOSLearningCircle
+//
+//  Created by Alberto Garcia on 13/06/23.
+//
+
+import Foundation
+
+enum HTTPError: Error {
+    case badURL
+    case basResponse
+    case decoding
+}
+
+struct Wrapper: Decodable {
+    let items: [Product]
+    
+    enum CodingKeys: String, CodingKey {
+        case items = "products"
+    }
+}
+
+struct Product: Decodable {
+    let title: String
+}
+
+final class HTTPClient {
+    static func load() async throws -> Wrapper {
+        guard let url = URL(string: "https://dummyjson.com/products") else {
+            throw HTTPError.badURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            throw HTTPError.basResponse
+        }
+        
+        let decoder = JSONDecoder()
+        guard let product = try? decoder.decode(Wrapper.self, from: data) else {
+            throw HTTPError.decoding
+        }
+        
+        return product
+    }
+    
+}
---
+++ iOSLearningCircle/SwiftUIViews/ProductListView.swift
@@ -0,0 +1,39 @@
+//
+//  ProductListView.swift
+//  iOSLearningCircle
+//
+//  Created by Alberto Garcia on 13/06/23.
+//
+
+import SwiftUI
+
+struct ProductListView: View {
+    @State var products: [Product] = []
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(0..<products.count, id: \.self) { index in
+                    Text("\(products[index].title)")
+                }
+            }
+            .navigationTitle(Text("Products"))
+            .onAppear {
+                Task {
+                    do {
+                        let wrapped = try await HTTPClient.load()
+                        products = wrapped.items
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ProductListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductListView()
+    }
+}
