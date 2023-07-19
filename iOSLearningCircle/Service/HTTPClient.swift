251b4a6f4dd23b067d68e43198211ae0f14ee21d
Alberto Garcia <alberto.garcia@wgu.edu>
2023-07-19 17:27:29 -0600

    Finish class 19/07/2023

--- iOSLearningCircle/Service/HTTPClient.swift
+++ iOSLearningCircle/Service/HTTPClient.swift
@@ -47,4 +47,43 @@ final class HTTPClient {
         
         return wrapper
     }
+    
+    static func loadPokedex() async throws -> [Pokemon] {
+        guard let url = URL(string: "https://pokedex-bb36f.firebaseio.com/pokemon.json") else {
+            throw HTTPError.badURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            throw HTTPError.basResponse
+        }
+        
+        let decoder = JSONDecoder()
+        if let data = data.parseData(removeString: "null,") {
+            guard let pokedex = try? decoder.decode([Pokemon].self, from: data) else {
+                throw HTTPError.decoding
+            }
+            
+            return pokedex
+        } else {
+            return []
+        }
+    }
+}
+
+struct Pokemon: Decodable {
+    let id: Int
+    let name: String
+    let imageUrl: String
+    let type: String
+}
+
+extension Data{
+    func parseData(removeString string: String ) -> Data? {
+        let dataAsString = String(data: self, encoding: .utf8)
+        let parsedDataString = dataAsString?.replacingOccurrences(of: string, with: "")
+        guard let data = parsedDataString?.data(using: .utf8) else {return nil}
+        return data
+    }
 }
--- iOSLearningCircle/UIKitControllers/ViewController/CollectionViewController.swift
+++ iOSLearningCircle/UIKitControllers/ViewController/CollectionViewController.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import SDWebImage
 
 class CollectionViewController: UICollectionViewController {
     let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    private var pokemons: [Pokemon] = []
     
     init() {
         super.init(collectionViewLayout: UICollectionViewFlowLayout())
@@ -21,17 +23,58 @@ class CollectionViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchPokedex()
         collectionView.register(CustomCell.self, forCellWithReuseIdentifier: "UICollectionViewCell")
     }
     
+    
+    
+    private func fetchPokedex() {
+        Task {
+            let pokemons = try await HTTPClient.loadPokedex()
+            self.pokemons = pokemons
+            collectionView.reloadData()
+        }
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension CollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        myArray.count
+        pokemons.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "UICollectionViewCell", for: indexPath) as! CustomCell
-        cell.label.text = "\(myArray[indexPath.item])"
+        cell.configure(pokemon: pokemons[indexPath.item])
         return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension CollectionViewController {
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .systemRed
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension CollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: (view.frame.width / 2) - 16, height: (view.frame.height / 6) - 16)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        16
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        16
     }
     
 }
@@ -47,14 +90,36 @@ class CustomCell: UICollectionViewCell {
         return lb
     }()
     
+    let imageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(systemName: "plus.square.fill")
+        return image
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: .zero)
         
-        backgroundColor = .systemBlue
+        backgroundColor = .white
         
-        addSubview(label)
-        label.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        label.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        addSubview(imageView)
+        imageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    func configure(pokemon: Pokemon) {
+        imageView.sd_setImage(with: URL(string: pokemon.imageUrl))
+        if pokemon.type == "poison" {
+            backgroundColor = .systemGreen
+        } else if pokemon.type == "fire" {
+            backgroundColor = .systemRed
+        } else if pokemon.type == "water" {
+            backgroundColor = .systemBlue
+        } else {
+            backgroundColor = .white
+        }
     }
     
     @available(*, unavailable)
